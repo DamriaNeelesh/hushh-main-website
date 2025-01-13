088391011d4a86f7b895b05d3efe89f68d2a5f71
DamriaNeelesh <damrianeelesh@gmail.com>
2025-01-13 16:54:03 +0530

    updating sitemap.xml

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.hush1one.com/",
+  siteUrl: "https://www.hushh.ai/",
   exclude: ["/icon.svg", "/apple-icon.png", "/manifest.webmanifest", "/tags/*"],
   generateRobotsTxt: true,
   generateIndexSitemap: false,
--- src/app/_components/authentication/appleSignIn.js
+++ src/app/_components/authentication/appleSignIn.js
@@ -1,18 +1,26 @@
 import config from "../config/config";
+
 export default async function appleSignIn() {
   try {
-    console.log('Starting Google Sign-In process...');
+    const redirectURL = process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000/developer-Api/on-boarding' // Development URL
+      : 'https://hushh.ai/developer-Api/on-boarding'; // Production URL
+
+    console.log('Starting Apple Sign-In process...');
     const { error } = await config.supabaseClient.auth.signInWithOAuth({
       provider: 'apple',
       options: {
-        redirectTo: 'http://localhost:3000/developer-Api/on-boarding',
+        redirectTo: redirectURL,
+        clientId: process.env.APPLE_CLIENT_ID, 
+        // redirectURI:'http://localhost:3000/developer-Api/on-boarding',
+        clientSecret: process.env.APPLE_CLIENT_SECRET,
       },
     });
 
     if (error) {
-      console.error('Error during Google Sign-In:', error.message);
+      console.error('Error during Apple Sign-In:', error.message);
     }
   } catch (error) {
-    console.error('Unexpected error during Google Sign-In:', error);
+    console.error('Unexpected error during Apple Sign-In:', error);
   }
 }
