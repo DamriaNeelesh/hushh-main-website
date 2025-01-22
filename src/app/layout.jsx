c875509b48fed2bb5995e9b1c3c23cebd032ede5
DamriaNeelesh <damrianeelesh@gmail.com>
2025-01-22 13:00:16 +0530

    Integrate Google Tag Manager into RootLayout
    - Added Google Tag Manager script for enhanced tracking and analytics.
    - Included a noscript fallback for users with JavaScript disabled.
    - Commented out the CookieConsent component for future use, maintaining layout integrity.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -95,6 +95,17 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Figtree:ital,wght@0,300..900;1,300..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5BP7FJ56');
+            `,
+          }}
+        />
       </head>
 
       <body
@@ -106,6 +117,8 @@ export default function RootLayout({ children }) {
         }}
         className={`${mulish.variable} font-sans`}
       >
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BP7FJ56"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <NextTopLoader
             color="red"
             initialPosition={0.08}
@@ -162,7 +175,7 @@ export default function RootLayout({ children }) {
           {" "}
           <div className="w-full">
             <Providers>
-              <CookieConsent/>
+              {/* <CookieConsent/> */}
               {children}
             </Providers>
           </div>
