6332f37a557551be0e2ebe09648d62360852edc2
DamriaNeelesh <damrianeelesh@gmail.com>
2025-02-21 10:36:27 +0530

    Refactor Home and Career page metadata and structure
    - Simplified Home page by removing redundant Head component
    - Added alternates canonical URL to Home page metadata
    - Updated Career page keywords to include job-related terms
    - Removed commented metadata in Career page

--- src/app/career/page.jsx
+++ src/app/career/page.jsx
@@ -11,6 +11,7 @@ export const metadata = {
     "HUSHH", "hushh", "Join Us", "Career", "Jobs", "Opportunity", "Personalized Experience", "Digital Footprint", "Gen AI", "Product that helps you keep track of your digital footprint", "Tired of being tracked online", "Track your own browsing activity", "Choose what data to collect", "Track your interests over time", "Export the collected data to your Hushh wallet", "Sell your data responsibly and get tailored services", "Streamlined Integration via Browser Companion", "Enables users to share their user activity in their Hushh Wallet", "Easy to organize and access important information", "Intelligent Insights and Recommendations", "Extension utilizes advanced algorithms and AI technology", "Latest trends with Gen AI", "Receive personalized recommendations", "Analyze the captured data and generate brilliant insights"
   ],
   canonical: "https://hushh.ai/career",
+  
   openGraph: {
     title: "Hushh | Careers",
     description:
@@ -40,10 +41,10 @@ const Career = () => {
           name="description"
           content="Join Us and build AI that advances the personalised data experiences"
         />
-        {/* <meta
+        <meta
           name="keywords"
-          content="Developer APIs, Hushh, Secure APIs, Consent-Driven Data, Data Integration, Application Development, Privacy Compliance"
-        /> */}
+          content="Developer APIs, Hushh, Secure APIs, Consent-Driven Data, Data Integration, Career, Join Hushh, Hushh Jobs, SDE, Product Management , Privacy Compliance"
+        />
         <meta property="og:title" content="Hushh | Careers" />
         <meta
           property="og:description"
